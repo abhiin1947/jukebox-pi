85fcbf1c8c79140c369d1f1ad7a1998698ed432a
abhiin1947 <abhiin1947@gmail.com>
2014-01-06 23:19:54 +0530

    a hell lot of changes

--- client.py
+++ client.py
@@ -1,33 +1,80 @@
 import threading
 import time
 import pygame
+import subprocess 
+import socket
 
-def aplayer( sock ): 
+#Properties
+WAIT_UNTIL_DATA = False
+USING_PYGAME = False
+USING_OMXPLAYER = True
+
+#Globals
+START_PLAYER = None
+GETDATATHREAD = None
+player = None
+sock = None
+
+
+
+def aplayer(): 
   file = "temp.mp3"
   #pygame.time.Clock().tick(100)
   pygame.mixer.music.stop()
+  pygame.mixer.music.set_volume(1.0)
   pygame.mixer.music.load(file)
   pygame.mixer.music.play()
   print "Playing .."
 
-import socket
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock.bind(('',8087))
-sock.listen(1)
-pygame.init()
-pygame.mixer.init()
-f = None
-while True:
-  connection, address = sock.accept()
+def omxplayer():
+  global player
+  if player != None:
+    player.stdin.write("q")
+    player.kill()
+    player = None
+  player = subprocess.Popen(["omxplayer", "temp.mp3"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def init():
+  print "Initializing Jukebox-pi..."
+  global START_PLAYER
+  if USING_PYGAME == True:
+    pygame.init()
+    pygame.mixer.init()
+    START_PLAYER = aplayer
+    print "Setting PYGAME as default player"
+  elif USING_OMXPLAYER == True:
+    START_PLAYER = omxplayer    
+    print "Setting OMXPLAYER as default player"
+
+def initsockets():
+  global sock
+  sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+  sock.bind(('',8087))
+  sock.listen(1)
+
+def getData(connection):
+  global f
   if f != None:
     f.close()
   f = open("temp.mp3","wb")
   f.write(connection.recv(38000))
-  aplayer(connection)
+  START_PLAYER()
   data = connection.recv(512)
   while len(data):
     f.write(data)
     data = connection.recv(512)
-#  while STATE == "playing":
-#    time.sleep(1)
-    
+
+def startdatathread(connection):
+  global GETDATATHREAD
+  GETDATATHREAD = threading.Thread(target=someFunc)
+  GETDATATHREAD.start()
+
+init()
+initsockets()
+
+
+f = None
+print "Jukebox-pi is good to go!"
+while True:  
+  connection, address = sock.accept()
+  getData(connection)
